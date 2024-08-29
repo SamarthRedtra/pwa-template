439d236fdac1459cdd21962e45157a870f34e2e3
RAVIBHARATHI P C <131471282+ravibharathi656@users.noreply.github.com>
2024-08-29 17:36:27 +0530

    Upadted With setting up apps on Mobile Like a PWA how it works

--- pwa/src/App.vue
+++ pwa/src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
     <router-view />
+    <InstallationPromt/>
   </div>
 </template>
+<script setup>
+import InstallationPromt from './form/components/InstallationPromt.vue';
+</script>
--- pwa/vite.config.js
+++ pwa/vite.config.js
@@ -2,10 +2,37 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import frappeui from 'frappe-ui/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [frappeui(), vue()],
+  plugins: [
+    frappeui(), 
+    vue(), 
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'PWA', 
+        short_name: 'PWA', 
+        start_url: '/pwa', 
+        display: 'standalone', 
+        background_color: '#ffffff', 
+        theme_color: '#ffffff', 
+        icons: [
+          {
+            src: '/assets/pwa_template/manifest/images_pwa_app.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/assets/pwa_template/manifest/images_pwa_app.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
@@ -15,6 +42,8 @@ export default defineConfig({
     outDir: `../${path.basename(path.resolve('..'))}/public/pwa`,
     emptyOutDir: true,
     target: 'es2015',
+    logLevel: 'debug',
+    chunkSizeWarningLimit: 1000,
   },
   optimizeDeps: {
     include: ['frappe-ui > feather-icons', 'showdown', 'engine.io-client'],
